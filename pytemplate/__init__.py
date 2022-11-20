477ba4ebdd09d9d8900083526c90bcab26330635
clemens33 <yongwu33@gmail.com>
2022-11-20 21:21:54 +0100

    added flake8 and pylint

--- pytemplate/__init__.py
+++ pytemplate/__init__.py
@@ -1,20 +1,46 @@
-import sys
+"""my test module"""
 
 
 def my_function(**kwargs) -> int:
-    r = 0
+    """asdfasdf
 
-    for k, v in kwargs.items():
-        r += v
+    Returns:
+        int: asdf
+    """
+    ret = 0
 
-    return r
+    for _, val in kwargs.items():
+        ret += val
+
+    return ret
+
+
+def my_bad_function() -> int:
+    """my bad function"""
+    aaa = 0
+    bbb = 2
+    ccc = 3
+
+    for i in range(10):
+        aaa += i
+        bbb += i
+        ccc += i
+
+    return aaa + bbb + ccc
+
+
+def func() -> str:
+    """a random function"""
+
+    return "hello world"
 
 
 def main():
+    """main function"""
     x = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10}
 
     print(my_function(**x))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
